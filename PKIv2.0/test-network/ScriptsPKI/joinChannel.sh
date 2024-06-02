eb8d64f8b8f6f1350a3a0e013bbe07d1e883e37b
EmersonSolis <emerson1524ricardo@gmail.com>
2024-06-02 17:57:21 -0500

    PKI v2.4

--- PKIv2.0/test-network/ScriptsPKI/joinChannel.sh
+++ PKIv2.0/test-network/ScriptsPKI/joinChannel.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-export FABRIC_CFG_PATH=${PWD}/../compose/docker/peercfg/CNE/
+# Set the configuration path
+export FABRIC_CFG_PATH=$(realpath ${PWD}/../compose/docker/peercfg/CNE)
+
+ls $FABRIC_CFG_PATH
+
+# Check if the core.yaml file exists
+if [ ! -f "$FABRIC_CFG_PATH/core.yaml" ]; then
+  echo "Error: core.yaml not found in $FABRIC_CFG_PATH"
+  exit 1
+fi
+
+
+# Define directories for genesis block and channel artifacts
+GENESIS_BLOCK_DIR=${PWD}/../configtx/system-genesis-block
+CHANNEL_ARTIFACTS_DIR=${PWD}/../configtx/channel-artifacts
+
+# Define peer environment variables for CNE
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="CNEMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/cne.com/peers/peer0.cne.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/cne.com/users/Admin@cne.com/msp/ 
+export CORE_PEER_TLS_ROOTCERT_FILE=$(realpath ${PWD}/../organizations/peerOrganizations/cne.com/peers/peer0.cne.com/tls/ca.crt)
+export CORE_PEER_MSPCONFIGPATH=$(realpath ${PWD}/../organizations/peerOrganizations/cne.com/users/Admin@cne.com/msp)
 export CORE_PEER_ADDRESS=peer0.cne.com:7051
 
-echo $CORE_PEER_MSPCONFIGPATH
 
-ls $CORE_PEER_MSPCONFIGPATH
+# Create the channel
+echo "Creating the channel..."
+sudo peer channel create -o orderer.cne.com:7050 -c mychannel -f $CHANNEL_ARTIFACTS_DIR/channel.tx --outputBlock $CHANNEL_ARTIFACTS_DIR/mychannel.block --tls --cafile $CORE_PEER_TLS_ROOTCERT_FILE
 
-# cd ../compose/docker/peercfg/CNE
-
-sudo peer channel create -o orderer.cne.com:7050 -c mychannel -f $(pwd)/../configtx/channel-artifacts/mychannel.tx --outputBlock $(pwd)/../configtx/channel-artifacts/mychannel.block --tls --cafile $(pwd)/../organizations/ordererOrganizations/cne.com/orderers/orderer.cne.com/tls/ca.crt
-
-# cd ../../../../ScriptsPKI/
-peer channel join -b $(pwd)/../configtx/channel-artifacts/mychannel.block --tls --cafile $(pwd)/../organizations/ordererOrganizations/cne.com/orderers/orderer.cne.com/tls/ca.crt
+# Join the CNE peer to the channel
+echo "Joining CNE to the channel..."
+sudo peer channel join -b $CHANNEL_ARTIFACTS_DIR/mychannel.block --tls --cafile $CORE_PEER_TLS_ROOTCERT_FILE
