4c5ba6cab0de8778073b445a6bcbb79143171dd8
EmersonSolis <emerson1524ricardo@gmail.com>
2024-06-04 08:22:23 -0500

    PKI v2.5.1

--- PKIv2.0/test-network/ScriptsPKI/joinChannel.sh
+++ PKIv2.0/test-network/ScriptsPKI/joinChannel.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Set the configuration path
-export FABRIC_CFG_PATH=$(realpath ${PWD}/../compose/docker/peercfg/CNE)
+export FABRIC_CFG_PATH=$(realpath ${PWD}/../compose/docker/peercfg/CNE);
 
 ls $FABRIC_CFG_PATH
 
 # Check if the core.yaml file exists
 if [ ! -f "$FABRIC_CFG_PATH/core.yaml" ]; then
-  echo "Error: core.yaml not found in $FABRIC_CFG_PATH"
-  exit 1
+  echo "Error: core.yaml not found in $FABRIC_CFG_PATH";
+  exit 1;
 fi
 
 
 # Define directories for genesis block and channel artifacts
-GENESIS_BLOCK_DIR=${PWD}/../configtx/system-genesis-block
-CHANNEL_ARTIFACTS_DIR=${PWD}/../configtx/channel-artifacts
+GENESIS_BLOCK_DIR=${PWD}/../configtx/system-genesis-block;
+CHANNEL_ARTIFACTS_DIR=${PWD}/../configtx/channel-artifacts;
 
 # Define peer environment variables for CNE
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="CNEMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$(realpath ${PWD}/../organizations/peerOrganizations/cne.com/peers/peer0.cne.com/tls/ca.crt)
-export CORE_PEER_MSPCONFIGPATH=$(realpath ${PWD}/../organizations/peerOrganizations/cne.com/users/Admin@cne.com/msp)
-export CORE_PEER_ADDRESS=peer0.cne.com:7051
+export CORE_PEER_TLS_ENABLED=true;
+export CORE_PEER_LOCALMSPID="CNEMSP";
+export CORE_PEER_TLS_ROOTCERT_FILE=$(realpath ${PWD}/../organizations/peerOrganizations/cne.com/peers/peer0.cne.com/tls/ca.crt);
+export CORE_PEER_MSPCONFIGPATH=$(realpath ${PWD}/../organizations/peerOrganizations/cne.com/users/Admin@cne.com/msp);
+export CORE_PEER_ADDRESS=peer0.cne.com:7051;
 
 # Define the orderer CA certificate
-ORDERER_CA=$(realpath ${PWD}/../organizations/ordererOrganizations/cne.com/orderers/orderer.cne.com/tls/ca.crt)
+ORDERER_CA=$(realpath ${PWD}/../organizations/ordererOrganizations/cne.com/orderers/orderer.cne.com/tls/ca.crt);
 
 # Create the channel
-echo "Creating the channel..."
-peer channel create -o orderer.cne.com:7050 -c mychannel -f $CHANNEL_ARTIFACTS_DIR/channel.tx --outputBlock $CHANNEL_ARTIFACTS_DIR/mychannel.block --tls --cafile $ORDERER_CA
+echo "Creating the channel...";
+peer channel create -o orderer.cne.com:7050 -c mychannel -f $CHANNEL_ARTIFACTS_DIR/channel.tx --outputBlock $CHANNEL_ARTIFACTS_DIR/mychannel.block --tls --cafile $ORDERER_CA;
 
 # Join the CNE peer to the channel
-export FABRIC_CFG_PATH=$(realpath ${PWD}/../compose/docker/peercfg/CNE)
-echo "Joining CNE to the channel..."
-sudo peer channel join -b $CHANNEL_ARTIFACTS_DIR/mychannel.block --tls --cafile $CORE_PEER_TLS_ROOTCERT_FILE
+export FABRIC_CFG_PATH=$(realpath ${PWD}/../compose/docker/peercfg/CNE);
+echo "Joining CNE to the channel...";
+peer channel join -b $CHANNEL_ARTIFACTS_DIR/mychannel.block --tls --cafile $CORE_PEER_TLS_ROOTCERT_FILE;
